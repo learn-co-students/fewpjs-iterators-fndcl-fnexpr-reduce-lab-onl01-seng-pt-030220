545059a67bd4a0961ce5ac5af4f9f4abbb5204ce
AHMED AFANDY <eng.computer1@yahoo.com>
2020-09-14 09:03:36 +0000

    Done.

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
 
-let totalBatteries = [1, 2, 3].reduce(function(total, element){ return element * 2 + total},19)
+let totalBatteries = batteryBatches.reduce(function(total, element){ return element * 1 + total})
